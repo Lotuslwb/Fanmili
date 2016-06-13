baa24355a028c8fa1ffeef96b833720c33d4ea0f
lotuslwb <837531387@qq.com>
2016-06-13 19:58:59 +0800

    sd

---
+++ fanmili/models/excelHandle.js
@@ -0,0 +1,44 @@
+var XLSX = require('xlsx');
+var path = require('path');
+
+var excel = path.join(__dirname, 'test.xlsx');
+
+var workbook = XLSX.readFile(excel);
+
+var sheetNames = workbook.SheetNames;
+
+var worksheet = workbook.Sheets[sheetNames[0]];
+
+var excelJSON = {};
+
+for (var cells in worksheet) {
+    // 不是以 ! 开头
+    if (cells.indexOf('!') != 0) {
+        var col = cells.substring(0, 1);
+        var row = parseInt(cells.substring(1));
+        var value = worksheet[cells].v;
+
+        if (!excelJSON[row]) {
+            excelJSON[row] = {};
+        }
+
+        var cellsJSON = excelJSON[row];
+
+        switch (col) {
+            //第一行 为name
+            case 'A':
+                cellsJSON['name'] = value;
+                break;
+            case 'B':
+                cellsJSON.tel = value;
+                break;
+            case 'C':
+                cellsJSON.others = value;
+                break;
+        }
+    }
+}
+//删除头部信息;
+excelJSON['1'] = null;
+
+module.exports = excelJSON;
--- fanmili/routes/telList.js
+++ fanmili/routes/telList.js
@@ -1,30 +1,49 @@
 var express = require('express');
 var router = express.Router();
+//数据库处理
 var Person = require('../models/DB');
+//excel JSON
+var excelJSON = require('../models/excelHandle');
+//excel Array
+var excelArray = [];
+
+for (var key in excelJSON) {
+    var item = excelJSON[key];
+    if (item) {
+        var obj = {
+            name: item.name || '',
+            tel: item.tel || '',
+            gender: '未知',
+            age: '真的是未知!!'
+        };
+        excelArray.push(obj);
+    }
+}
 
 /* GET home page. */
 var user = new Person();
-var data = {
-    name: '李文彬',
-    tel: '99999',
-    gender: '男',
-    age: '12'
-}
 
-user.add(data, function (err) {
-    user.find({}, function (err, docs) {
-        var data = {
-            title: 'telList', telList: docs
-        }
-        router.get('/', function (req, res, next) {
-            res.render('list', data);
-        });
+user.User.collection.insert(excelArray, function (err, docs) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('批量插入成功!');
+        user.find({}, function (err, docs) {
+            var data = {
+                title: 'telList', telList: docs
+            }
+            router.get('/', function (req, res, next) {
+                res.render('list', data);
+            });
 
 
-        router.get('/test', function (req, res, next) {
-            res.json(data);
-        });
-    })
+            router.get('/test', function (req, res, next) {
+                res.json(data);
+            });
+        })
+    }
+
 });
 
 
